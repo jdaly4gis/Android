3a5177e8e0d2fda8c0927f828dbcf9aacd8009bb
John Daly <jdaly@panix.com>
2016-05-24 14:03:57 -0400

    Adding and commit of YouthServices app

---
+++ YouthServices/app/src/main/java/gov/townofsouthamptonny/android/youthservices/SingleFragmentActivity.java
@@ -0,0 +1,35 @@
+package gov.townofsouthamptonny.android.youthservices;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JDaly on 2/2/2016.
+ */
+public abstract class  SingleFragmentActivity extends AppCompatActivity {
+
+    @LayoutRes
+    protected int getLayoutResId()  {
+        return R.layout.activity_fragment;
+    }
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        //setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null)  {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+}
